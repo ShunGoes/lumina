b08034ea1d1430492c1cbbc08b99359d2072476a
shunGoes <shunnode@gmail.com>
2024-03-18 22:46:14 +0100

    ADDED FB BTN FOR TEST

--- src/pagges/tinder/test.tsx
+++ src/pagges/tinder/test.tsx
@@ -1,20 +1,19 @@
+// @ts-ignore
+import FacebookLogin from 'react-facebook-login'
 
 const Test = () => {
-  const handleLogin = () => {
-    const redirectUri = encodeURIComponent('http://localhost:5173/register');
-    const clientId = encodeURIComponent('YOUR_CLIENT_ID');
-    const responseType = 'code';
-    const scope = encodeURIComponent('profile email'); // Specify required scopes
-
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
-  };
-
+  const responseFacebook = (response: any) => {
+    console.log(response);
+  }
   return (
     <div>
-      <h1>Test Page</h1>
-      <button onClick={handleLogin}>Test with Google</button>
+      <FacebookLogin
+    appId={import.meta.env.VITE_FACEBOOK_APP_I}
+    autoLoad={true}
+    fields="name,email,picture"
+    callback={responseFacebook} />,
     </div>
-  );
-};
+  )
+}
 
-export default Test;
+export default Test
