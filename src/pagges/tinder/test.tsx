0da8fd2d74be851afd60f16e890e3bed8ba9ebee
shunGoes <shunnode@gmail.com>
2024-03-19 11:26:28 +0100

    removed library import

--- src/pagges/tinder/test.tsx
+++ src/pagges/tinder/test.tsx
@@ -1,17 +1,9 @@
-// @ts-ignore
-import FacebookLogin from 'react-facebook-login'
 
 const Test = () => {
-  const responseFacebook = (response: any) => {
-    console.log(response);
-  }
+ 
   return (
     <div>
-      <FacebookLogin
-    appId={import.meta.env.VITE_FACEBOOK_APP_I}
-    autoLoad={true}
-    fields="name,email,picture"
-    callback={responseFacebook} />,
+   
     </div>
   )
 }
