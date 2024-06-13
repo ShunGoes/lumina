67af5f978f2d18237ff064ff285cbd7d0ef4f408
ayebakuroombu <ayebakuro@investbamboo.com>
2024-06-13 22:50:27 +0100

    Changed url

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://lumina-wszdbcrgga-uc.a.run.app/api";
+const baseUrl = "https://lumina-k9dt.onrender.com/api";
 // const baseUrl = "http://localhost:8000/api";
 
 const client = axios.create({
